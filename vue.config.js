9ca07afb793dc888b2a4c99b15f1d750597d0aeb
Patrick Miguel <patrickmiguel286@gmail.com>
2024-10-26 13:52:44 -0300

    mensagem

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js', // Caminho para o seu arquivo de entrada principal
+      title: 'Saber Mais' // Novo título desejado
+    }
+  }
+});
